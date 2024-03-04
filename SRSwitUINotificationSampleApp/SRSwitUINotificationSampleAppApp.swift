ab41af53915b3f54d523a67fbf686e2bcfb7f115
Sahan Ravindu <sahanrv@outlook.com>
2024-03-04 06:32:46 +0530

    Initial Commit

---
+++ SRSwitUINotificationSampleApp/SRSwitUINotificationSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SRSwitUINotificationSampleAppApp.swift
+//  SRSwitUINotificationSampleApp
+//
+//  Created by Sahan Ravindu on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SRSwitUINotificationSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
